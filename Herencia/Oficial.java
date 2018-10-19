77a7fc14b2ee11f7141234896a3240d3807fc5ae
JuanFMonfort <43619056+JuanFMonfort@users.noreply.github.com>
2018-10-19 19:46:53 +0400

    Create Oficial.java

---
+++ Herencia/Oficial.java
@@ -0,0 +1,13 @@
+public class Oficial extends Operario{
+
+	public Oficial(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+" -> Oficial";
+	}
+	
+}
