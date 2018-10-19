53daa4cca8bd8cf5b7c8022fc7450a7bab1b5b25
JuanFMonfort <43619056+JuanFMonfort@users.noreply.github.com>
2018-10-19 19:47:42 +0400

    Create Tecnico.java

---
+++ Herencia/Tecnico.java
@@ -0,0 +1,13 @@
+public class Tecnico extends Operario{
+
+	public Tecnico(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()+" -> Tecnico";
+	}
+	
+}
